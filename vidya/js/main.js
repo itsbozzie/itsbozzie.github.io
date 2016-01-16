6b0f52b1f35e7bd877f1475a0d8ad92793462cfd
Boris Guyon-Pelfrene <bozguyon@hotmail.com>
2016-01-16 23:02:59 +0000

    Renamed to vidya-ui, split css files

--- vidya/js/main.js
+++ vidya/js/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   //fades out all elements in the body
   $('.coin-button').on('click', function(){
+    $(this).addClass('animated bounce');
     $('body *').fadeOut(2000);
+    //delays the redirect to new URL so that the fadeOut animation can play
+    setTimeout(function() { window.location.href = "page-2.html";}, 5000);
   });
-  //delays the redirect to new URL so that the fadeOut animation can play
-  setTimeout(function() { window.location.href = "page-2.html";}, 5000);
 });
