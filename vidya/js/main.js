ac5046c3472395a27de474c28b907695bbeec915
Boris Guyon-Pelfrene <bozguyon@hotmail.com>
2016-01-16 19:18:57 +0000

    Made first page
    Made it so clicking on the coin will fade out all elements in body,
    then redirect to new page

--- vidya/js/main.js
+++ vidya/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-
-
-
-
+  //fades out all elements in the body
+  $('.coin-button').on('click', function(){
+    $('body *').fadeOut(2000);
+  });
+  //delays the redirect to new URL so that the fadeOut animation can play
+  setTimeout(function() { window.location.href = "page-2.html";}, 5000);
 });
