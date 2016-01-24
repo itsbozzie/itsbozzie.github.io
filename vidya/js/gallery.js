0c90cb51341eaada0cefcfac7f4e13b5c9d044e9
Boris Guyon-Pelfrene <bozguyon@hotmail.com>
2016-01-24 21:16:49 +0000

    Added more gallery items, made gallery page more (width)responsive

--- vidya/js/gallery.js
+++ vidya/js/gallery.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
-  // On Focus
-  $('#auto-complete').on('focus', function() {
-    $('.gallery-header-text').addClass('is-focussed')
-  // On Blur (non-focus)
-  }).on("blur", function(){
-    $('.gallery-header-text').removeClass('is-focussed')
-  // On Hover
-}).on('mouseenter', function(){
-    $('.gallery-header-text').addClass('is-hovered')
-  // On MouseLeave
-  }).on('mouseleave', function(){
-    $('.gallery-header-text').removeClass('is-hovered')
+    // On Focus
+    $('#auto-complete').on('focus', function() {
+      $('.gallery-header-text').addClass('is-focussed')
+    // On Blur (non-focus)
+    }).on("blur", function(){
+      $('.gallery-header-text').removeClass('is-focussed')
+    // On Hover
+  }).on('mouseenter', function(){
+      $('.gallery-header-text').addClass('is-hovered')
+    // On MouseLeave
+    }).on('mouseleave', function(){
+      $('.gallery-header-text').removeClass('is-hovered')
+  });
+
+  var gameItem = $('.game-item');
+
+  $.each(gameItem, function(i, item) {
+      setTimeout(function() {
+          $(item).fadeIn(400);
+      }, 200 * i);
   });
 
 });
